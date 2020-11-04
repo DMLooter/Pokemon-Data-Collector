fec5a5944f2b153406cf4450626139c35861badc
DMLooter <verbanmike@gmail.com>
2020-11-04 09:51:27 -0600

    Initial commit

---
+++ src/main/java/com/verban/PokemonDataCollector/App.java
@@ -0,0 +1,13 @@
+package com.verban.PokemonDataCollector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
